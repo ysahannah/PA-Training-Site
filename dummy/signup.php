399708339c6b7db45c797e5e3e06536100f92ceb
KingOfMages <zerolover3202@gmail.com>
2024-03-13 23:31:15 +0800

    Signup.html is now linked to the database, any info entered will register

--- dummy/signup.php
+++ dummy/signup.php
@@ -15,11 +15,45 @@ if ($conn->connect_error){
 }
 
 //checking if form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if (isset($_POST['submit'])){
   //RETRIVE FORM
-  $username = $_POST[];
-  $password = $_POST[]; 
-  $email = $_POST["email"];
- 
-  $sql = "INSERT INTO users (username, pass, email) VALUES ('$username', '$password', '$email')"
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $cpass = $_POST['cpass']; 
+  $email = $_POST["Email"];
+
+
+$sql="select * from users where username='$username'";
+$result = mysqli_query($conn, $sql);
+$count_user = mysqli_num_rows($result);
+
+$sql="select * from users where email='$email'";
+$result = mysqli_query($conn, $sql);
+$count_email = mysqli_num_rows($result);
+
+//searching if there is identical names/emails, also hashing the password for security
+if($count_user == 0 && $count_email == 0){  
+  if($password==$cpass){
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$hash' , '$email')";
+    $result = mysqli_query($conn, $sql);
+    if($result){
+      echo 'SIGN UP SUCCESSFUL!';
+    }
+  }
+  else{
+    echo 'Passwords do not match' ;
+  }
 }
+  else{
+    if($count_user>0){
+      echo 'This user already exists!';
+    }
+    if($count_email>0){
+      echo 'This email already exists!';
+    }
+  }
+
+
+}
+?>
