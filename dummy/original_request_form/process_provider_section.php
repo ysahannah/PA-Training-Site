cec3f307c436736bac9dc23bd382fd3c85baffcf
ysahannah <hleguro8@gmail.com>
2024-03-14 01:30:24 +0800

    Updated the process_provider_section.php

--- dummy/original_request_form/process_provider_section.php
+++ dummy/original_request_form/process_provider_section.php
@@ -1,8 +1,6 @@
 <?php
-session_start();
+error_reporting(E_ALL);
 
-
-?><?php
 session_start();
 
 $servername = "localhost";
@@ -10,29 +8,31 @@ $username = "root";
 $password = "";
 $dbname = "covermymeds";
 
-$conn = new mysqli( $servername, $username, $password, $dbname );
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve form data
     $npi = $_POST["npi"];
     $provider_firstName = $_POST["provider_firstName"];
     $provider_lastName = $_POST["provider_lastName"];
     $street2 = $_POST["street2"];
     $street = $_POST["street"];
     $provider_state = $_POST["provider_state"];
-    $address_line = $_POST["address_line"];
     $city = $_POST["city"];
-    $state = $_POST["state"];
     $zip_code = $_POST["zip_code"];
     $phone = $_POST["phone"];
     $fax = $_POST["fax"];
 
-    $provider_sql = "INSERT INTO patient_information (npi, provider_firstName, provider_lastName, street2, street, address_line, city, state, zip_code, phone, fax) VALUES ('$npi', '$provider_firstName', '$provider_lastName', '$street2', '$street', '$address_line', '$city', '$state', '$zip_code', '$phone', '$fax')";
+    // Prepare SQL statement
+    $provider_sql = "INSERT INTO patient_information (npi, provider_firstName, provider_lastName, street2, street, city, state, zip_code, phone, fax) 
+                     VALUES ('$npi', '$provider_firstName', '$provider_lastName', '$street2', '$street', '$city', '$provider_state', '$zip_code', '$phone', '$fax')";
 
+    // Execute SQL statement
     if ($conn->query($provider_sql) === TRUE) {
         echo "Provider information inserted successfully";
         exit();
