2e170052c308c88796aa38f0147dcadb45c14e08
ysahannah <hleguro8@gmail.com>
2024-03-15 01:49:38 +0800

    original_request_form/index.html is now working. there are minor issues need to be fixed

--- dummy/original_request_form/process_patient_section.php
+++ dummy/original_request_form/process_patient_section.php
@@ -1,44 +1,38 @@
 <?php
-session_start();
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "covermymeds";
 
-$conn = new mysqli( $servername, $username, $password, $dbname );
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $prefix = $_POST["prefix"];
-    $firstName = $_POST["firstName"];
-    $middle_name = $_POST["middle_name"];
-    $lastName = $_POST["lastName"];
-    $suffix = $_POST["suffix"];
-    $birthday = $_POST["birthday"];
-    $gender = $_POST["gender"];
-    $member_id = $_POST["member_id"];
-    $street = $_POST["street"];
-    $street2 = $_POST["street2"];
-    $city = $_POST["city"];
-    $zip_code = $_POST["zip_code"];
-    $phone = $_POST["phone"];
-    $provider_state = $_POST["provider_state"];
+$prefix = $_POST['prefix'];
+$firstName = $_POST['firstName'];
+$middle_name = $_POST['middle_name'];
+$lastName = $_POST['lastName'];
+$suffix = $_POST['suffix'];
+$birthday = $_POST['birthday'];
+$gender = $_POST['gender'];
+$member_id = $_POST['member_id'];
+$street = $_POST['street'];
+$street2 = $_POST['street2'];
+$city = $_POST['city'];
+$state = $_POST['provider_state'];
+$zip_code = $_POST['zip_code'];
+$phone = $_POST['phone'];
 
-    $patient_sql = "INSERT INTO patient_information (prefix, firstName, middle_name, lastName, suffix, birthday, gender, member_id, street, street2, city, zip_code, phone, provider_state) VALUES ('$prefix', '$firstName', '$middle_name', '$lastName', '$suffix', '$birthday', '$gender', '$member_id', '$street', '$street2', '$city', '$zip_code', '$phone', '$patient_insurance_state')";
+$sql = "INSERT INTO patient_information (prefix, firstName, middle_name, lastName, suffix, birthday, gender, member_id, street, street2, city, provider_state, zip_code, phone)
+        VALUES ('$prefix', '$firstName', '$middle_name', '$lastName', '$suffix', '$birthday', '$gender', '$member_id', '$street', '$street2', '$city', '$state', '$zip_code', '$phone')";
 
-    if ($conn->query($patient_sql) === TRUE) {
-        echo "Patient information inserted successfully";
-        exit();
-    } else {
-        echo "Error: " . $patient_sql . "<br>" . $conn->error;
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the database connection
 $conn->close();
 ?>
--- dummy/original_request_form/process_provider_section.php
+++ dummy/original_request_form/process_provider_section.php
@@ -1,8 +1,4 @@
 <?php
-error_reporting(E_ALL);
-
-session_start();
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,36 +6,32 @@ $dbname = "covermymeds";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve form data
-    $npi = $_POST["npi"];
-    $provider_firstName = $_POST["provider_firstName"];
-    $provider_lastName = $_POST["provider_lastName"];
-    $street2 = $_POST["street2"];
-    $street = $_POST["street"];
-    $provider_state = $_POST["provider_state"];
-    $city = $_POST["city"];
-    $zip_code = $_POST["zip_code"];
-    $phone = $_POST["phone"];
-    $fax = $_POST["fax"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $npi = $_POST['npi'];
+    $provider_firstName = $_POST['provider_firstName'];
+    $provider_lastName = $_POST['provider_lastName'];
+    $street = $_POST['street'];
+    $street2 = $_POST['street2'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip_code = $_POST['zip_code'];
+    $phone = $_POST['phone'];
+    $fax = $_POST['fax'];
+    $review = $_POST['review'];
 
-    // Prepare SQL statement
-    $provider_sql = "INSERT INTO patient_information (npi, firstName, lastName, street2, street, city, patient_insurance_state, zip_code, phone, fax) VALUES ('$npi', '$provider_firstName', '$provider_lastName', '$street2', '$street', '$city', '$provider_state', '$zip_code', '$phone', '$fax')";
+    $sql = "INSERT INTO provider (npi, provider_firstName, provider_lastName, street, street2, city, state, zip_code, phone, fax, review)
+    VALUES ('$npi', '$provider_firstName', '$provider_lastName', '$street', '$street2', '$city', '$state', '$zip_code', '$phone', '$fax', '$review')";
 
-    // Execute SQL statement
-    if ($conn->query($provider_sql) === TRUE) {
-        echo "Provider information inserted successfully";
-        exit();
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
     } else {
-        echo "Error: " . $provider_sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
