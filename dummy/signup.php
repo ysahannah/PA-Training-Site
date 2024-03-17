94e0c861c7d6861912fdfd378b734912361fe66d
Joshua Delos Santos <zerolover3202@gmail.com>
2024-03-17 17:58:52 +0800

    Changed PHP code for testing.

--- dummy/signup.php
+++ dummy/signup.php
@@ -20,22 +20,20 @@ if (isset($_POST['submit'])){
   $username = $_POST['username'];
   $password = $_POST['password'];
   $cpass = $_POST['cpass']; 
-  $email = $_POST["Email"];
+
 
 
 $sql="select * from users where username='$username'";
 $result = mysqli_query($conn, $sql);
 $count_user = mysqli_num_rows($result);
 
-$sql="select * from users where email='$email'";
-$result = mysqli_query($conn, $sql);
-$count_email = mysqli_num_rows($result);
+
 
 //searching if there is identical names/emails, also hashing the password for security
-if($count_user == 0 && $count_email == 0){  
+if($count_user == 0){  
   if($password==$cpass){
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$hash' , '$email')";
+    //$hash = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
     $result = mysqli_query($conn, $sql);
     if($result){
       // Redirect to login page after successful sign-up
@@ -51,11 +49,9 @@ if($count_user == 0 && $count_email == 0){
     if($count_user>0){
       echo 'This user already exists!';
     }
-    if($count_email>0){
-      echo 'This email already exists!';
     }
   }
 
 
-}
+
 ?>
