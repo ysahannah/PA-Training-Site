54e51973d538fc9b57598c659d04765191a9201b
xed-kun <xedricllonel12@gmail.com>
2024-03-21 05:28:03 +0800

    admin_page(in progress)

---
+++ dummy/admin_page-create.php
@@ -0,0 +1,56 @@
+<?php
+$username = "";
+$password = "";
+
+if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    do{
+        if ( empty($username))
+
+    } while (false);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>NEW CLIENT</h2>
+        <form method="post">
+            <div class="row nb-3">
+                <label class="col-sm-3 col-form-label">Username</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
+                </div>
+            </div>
+
+            <div class="row nb-3">
+                <label class="col-sm-3 col-form-label">Password</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="password" value="<?php echo $password; ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outlime-priimary" href="../dummy/admin_page-inside.php" role="button">Cancel</a>
+                </div>
+            </div>
+            
+
+        </form>
+    </div>
+</body>
+</html>
---
+++ dummy/admin_page-inside.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../dummy/new_photos/LG.png">
+    <title>Admin Page</title>
+    <!--<link rel="stylesheet" href="../dummy/admin_page.css">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body class="bg-dark">
+    <!--Logo-->
+   <!-- <header></header>-->
+
+    <div class="container my-5">
+       <h2>LIST OF USERS</h2>
+       <a class="btn btn-primary" href="" role="button">New CLIENT</a>
+       <br>
+       <table class="table">
+            <thead>
+                <tr>
+                    <th>USERNAME</th>
+                    <th>PASSWORD</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "pa_training_site";
+
+                //Create Connection
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                //check connection
+                if ($connection->connect_error) {
+                    die("Connection failed: " . $connection->connect_error);
+                }
+
+                //read all row from database table
+                $sql = "SELECT *FROM users";
+                $result = $connection->query($sql);
+
+                if (!$result) {
+                    die("Invalid query: " . $connection->error);
+                }
+
+                //read data of each row
+                while($row = $result->fetch_assoc()){
+                    echo "
+                    <tr>
+                        <td>$row[username]</td>
+                        <td>$row[password]</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm' href='edit.php?id=$row'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='delete.php?id=$row'>Delete</a>
+                        </td>
+                    </tr>
+                    ";
+                }
+
+                ?>
+                
+               
+            </tbody>
+       </table>
+    </div>
+    
+</body>
+</html>
